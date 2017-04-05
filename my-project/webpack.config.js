083b546fc32b15965f3ce5792f81c062e828dc4a
webcainiao <2401800893@qq.com>
2017-04-05 17:04:11 +0800

    sss

--- my-project/webpack.config.js
+++ my-project/webpack.config.js
@@ -1,5 +1,7 @@
 var path = require('path')
 var webpack = require('webpack')
+var ExtractTextPlugin = require('extract-text-webpack-plugin')
+var ExtractTextPluginScss = new ExtractTextPlugin('mestyle.css')
 
 module.exports = {
   entry: './src/main.js',
@@ -12,17 +14,36 @@ module.exports = {
     rules: [
       {
         test: /\.vue$/,
-        loader: 'vue-loader',
-        options: {
-          loaders: {
-            // Since sass-loader (weirdly) has SCSS as its default parse mode, we map
-            // the "scss" and "sass" values for the lang attribute to the right configs here.
-            // other preprocessors should work out of the box, no loader config like this necessary.
-            'scss': 'vue-style-loader!css-loader!sass-loader',
-            'sass': 'vue-style-loader!css-loader!sass-loader?indentedSyntax'
-          }
+        // loader: 'vue-loader',
+        // options: {
+        //   loaders: {
+        //     // Since sass-loader (weirdly) has SCSS as its default parse mode, we map
+        //     // the "scss" and "sass" values for the lang attribute to the right configs here.
+        //     // other preprocessors should work out of the box, no loader config like this necessary.
+        //     // 'scss': 'vue-style-loader!css-loader!sass-loader',
+        //     // 'sass': 'vue-style-loader!css-loader!sass-loader?indentedSyntax'
+        //     // scss: ExtractTextPlugin.extract({
+        //     //   use: 'css-loader!sass-loader',
+        //     //   fallback: 'vue-style-loader'
+        //     // })和下面形式是一样的and plugins
+            // scss: ExtractTextPluginScss.extract({
+            //   use: 'css-loader!sass-loader',
+            //   fallback: 'vue-style-loader'
+            // })
+        //   }
+        use: [
+          {loader: 'vue-loader',
+          options: {
+            loaders: {
+                scss: ExtractTextPluginScss.extract({
+                use: 'css-loader!sass-loader',
+                fallback: 'vue-style-loader'
+              })
+            }
+          }}
+        ]
           // other vue-loader options go here
-        }
+        // }
       },
       {
         test: /\.js$/,
@@ -50,7 +71,11 @@ module.exports = {
   performance: {
     hints: false
   },
-  devtool: '#eval-source-map'
+  devtool: '#eval-source-map',
+  plugins: [
+    // new ExtractTextPlugin('mystyle.css')
+    ExtractTextPluginScss
+  ]
 }
 
 if (process.env.NODE_ENV === 'production') {
--- vue-webpack/src/components/Hello.vue
+++ vue-webpack/src/components/Hello.vue
@@ -1,15 +1,16 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{msg}}</h1>
-    <h2>Essential Links jjj</h2>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
     <ul>
       <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
       <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
       <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
       <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
+      <br>
+      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
     </ul>
-    <h2 class="hh2">Ecosystem</h2>
+    <h2>Ecosystem</h2>
     <ul>
       <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
       <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
@@ -21,7 +22,7 @@
 
 <script>
 export default {
-  name: 'app',
+  name: 'hello',
   data () {
     return {
       msg: 'Welcome to Your Vue.js App'
@@ -30,16 +31,8 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
 h1, h2 {
   font-weight: normal;
 }
@@ -56,8 +49,5 @@ li {
 
 a {
   color: #42b983;
-}
-.hh2 {
-  color: red;
 }
 </style>
