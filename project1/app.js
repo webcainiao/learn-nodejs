e2bf5b78eb77d1c35879c1856dbfdd581eda7c75
webcainiao <2401800893@qq.com>
2017-04-12 15:38:04 +0800

    df

--- project1/app.js
+++ project1/app.js
@@ -7,16 +7,20 @@ var webpackConfig = process.env.NODE_ENV==='development'
 var historyApiFallback = require('connect-history-api-fallback');
 var app = express();
 
-var compiler = webpack(webpackConfig);
-var devMiddleware = require('webpack-dev-middleware')(compiler,{
-	publicPath: '/',
-	stats: {
-		colors: true,
-		chunks: false
-	}
-});
-
 app.use(historyApiFallback());
-app.use(devMiddleware);
-app.use(require('webpack-hot-middleware')(compiler))
+if (app.get('env') == 'development') {
+	var compiler = webpack(webpackConfig);
+	var devMiddleware = require('webpack-dev-middleware')(compiler,{
+		publicPath: '/',
+		stats: {
+			colors: true,
+			chunks: false
+		}
+	});
+
+	app.use(devMiddleware);
+	app.use(require('webpack-hot-middleware')(compiler))
+} else {
+	app.use(express.static(path.resolve(__dirname,'dist')))
+}
 module.exports = app;
