edd8379f6d8881f88e6cead50bd9d1aa0be5904e
webcainiao <2401800893@qq.com>
2017-05-30 17:57:06 +0800

    2

---
+++ vue-webpack2/src/utils/filter.js
@@ -0,0 +1,12 @@
+export const formatDate = str => {
+  if (!str) {
+    return '';
+  }
+  var date = new Date(str);
+  var time = new Date().getTime() - date.getTime();
+  if (time < 0) {
+    return '';
+  } else if () {
+
+  }
+}
