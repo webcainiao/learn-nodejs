2e30dc97443a7950e3c481f1da50b1b367b9c203
webcainiao <2401800893@qq.com>
2017-06-06 15:40:25 +0800

    fffff

--- vue-webpack2/src/views/login.vue
+++ vue-webpack2/src/views/login.vue
@@ -1,10 +1,46 @@
 <template>
-
+  <div>
+    <section class="login-page">
+      <div class="input-token">
+        <input class="token" type="text" placeholder="请输入正确的token" maxlength="36" v-model="accesstoken">
+      </div>
+      <div class="login-btn">
+        <button class="btn" @click="login">登录</button>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
+  import api from '../fetch/api'
   export default {
-
+    data () {
+      return {
+        accesstoken: ''
+      }
+    },
+    methods: {
+      ...mapActions({
+        setUserInfo: 'setUserInfo'
+      }),
+      login () {
+        if (!this.accesstoken) {
+          return console.log('请输入正确的token')
+        }
+        api.Login(this.accesstoken).then(res => { //res是对象
+          if (res.success) {
+            let userInfo = Object.assign({accesstoken:this.accesstoken}, res)
+            // 将返回的信息存入状态管理中,进而存储用户信息
+            this.setUserInfo(userInfo)
+            this.$route.go(-1)
+          }
+        }).catch(error => {
+          console.log(error)
+          console.log(error.responseText)
+          console.log(JSON.parse(error.responseText))
+        })
+      }
+    }
   }
 </script>
 
--- vue-webpack2/src/vuex/modules/user.js
+++ vue-webpack2/src/vuex/modules/user.js
@@ -1,9 +1,28 @@
-// import types from '../types'
+import types from '../types'
 
-const state = {}
+const state = {
+  loginStatus: JSON.parse(localStorage.getItem('loginStatus')) || false,
+  userInfo: JSON.parse(localStorage.getItem('user')) || {}
+}
 const getters = {}
-const mutations = {}
-const actions = {}
+const mutations = {
+  [types.SET_USER_INFO] (state, res) {
+    state.userInfo = res
+  },
+  [types.SET_LOGIN_STATUS] (state, status) {
+    state.loginStatus = status
+  }
+}
+const actions = {
+  setUserInfo ({commit}, res) {
+    // 浏览器存储用户信息，保存json字符串信息
+    localStorage.setItem(user, JSON.stringify(res)) // 将res解析为json字符串
+    localStorage.setItem(loginStatus, true) // loginStatus: 'true'字符串
+    // 将用户信息存入状态管理中，供其他路由组件使用
+    commit(types.SET_USER_INFO, res)
+    commit(types.SET_LOGIN_STATUS, true)
+  }
+}
 
 export default {
   state,
